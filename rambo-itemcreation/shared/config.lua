8d1fb176f505d51f788343824532029e5029c528
bigramb0 <84361647+bigramb0@users.noreply.github.com>
2023-01-03 13:54:50 -0500

    Add files via upload

---
+++ rambo-itemcreation/shared/config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+Config.Items = {}
+
+-- boxbomb by Rambo
+Config.Items['box'] = {
+	item = 'box',
+	label = 'A cardboard box',
+	weight = 10000,
+	image = 'placeholder.png',
+	unique = true,
+	useable = false,
+	shouldClose = false,
+	combinable = nil,
+	description = 'Its a box!',
+}
